face7256649592a6b8af72a247805263c824413a
marco76tv <marco.sottana@gmail.com>
2019-08-14 10:11:21 +0200

    .

--- Models/Policies/XotBasePolicy.php
+++ Models/Policies/XotBasePolicy.php
@@ -96,6 +96,50 @@ abstract class XotBasePolicy {
 
         return false;
     }
+    
+    /**
+     * Determine whether the user can delete the DocDummyModel.
+     *
+     * @param  \Modules\LU\Models\User  $user
+     * @param  \Modules\Blog\Models\Privacy  $dummyModel
+     * @return mixed
+     */
+    public function delete(User $user, Post $post)
+    {
+        if ($post->created_by == $user->handle) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine whether the user can restore the DocDummyModel.
+     *
+     * @param  \Modules\LU\Models\User  $user
+     * @param  \Modules\Blog\Models\Privacy  $dummyModel
+     * @return mixed
+     */
+    public function restore(User $user, Post $post)
+    {
+        if ($post->created_by == $user->handle) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the DocDummyModel.
+     *
+     * @param  \Modules\LU\Models\User  $user
+     * @param  \Modules\Blog\Models\Privacy  $dummyModel
+     * @return mixed
+     */
+    public function forceDelete(User $user, Post $post)
+    {
+        //
+    }
 
     public function detach(User $user, $post){
         if ($post->created_by == $user->handle || $post->updated_by == $user->handle) {
@@ -108,6 +152,29 @@ abstract class XotBasePolicy {
     public function clone(User $user, $post){
         return true;
     }
+    /**
+     * Determine whether the user can view any DocDummyPluralModel.
+     *
+     * @param  \Modules\LU\Models\User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the DocDummyModel.
+     *
+     * @param  \Modules\LU\Models\User  $user
+     * @param  \Modules\Blog\Models\Privacy  $dummyModel
+     * @return mixed
+     */
+    public function view(User $user, Post $post)
+    {
+        //
+    }
+
 
 
 }
