50e1a485d313f1cedbc0fb55b4175b6e1345c990
marco76tv <marco.sottana@gmail.com>
2019-09-30 10:54:13 +0200

    .

---
+++ Contracts/UserContract.php
@@ -0,0 +1,11 @@
+<?php
+namespace Modules\Xot\Contracts;
+
+interface UserContract {
+	/*
+	public function isSuperAdmin();
+	public function name();
+	public function areas();
+	public function avatar();
+	*/
+}
