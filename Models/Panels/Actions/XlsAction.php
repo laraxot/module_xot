98f1564f786f6aab9c5188479a7c027be43b7f71
marco76tv <marco.sottana@gmail.com>
2019-12-10 14:13:53 +0100

    .

--- Models/Panels/Actions/XlsAction.php
+++ Models/Panels/Actions/XlsAction.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Modules\Xot\Models\Panels\Actions;
 
 //-------- models -----------
@@ -6,16 +7,16 @@ namespace Modules\Xot\Models\Panels\Actions;
 //-------- services --------
 use Modules\Xot\Services\ArrayService;
 
-
 //-------- bases -----------
 
 class XlsAction extends XotBasePanelAction {
-	public $name = 'xls'; //name for calling Action
+    public $name = 'xls'; //name for calling Action
     public $onContainer = true; //onlyContainer
     public $icon = '<i class="far fa-file-excel fa-1x"></i>';
 
-    public function handle(){
-    	$data=($this->rows->get()->toArray());
-    	return ArrayService::toXls(['data'=>$data,'filename'=>'test']);
+    public function handle() {
+        $data = ($this->rows->get()->toArray());
+
+        return ArrayService::toXls(['data' => $data, 'filename' => 'test']);
     }
 }
