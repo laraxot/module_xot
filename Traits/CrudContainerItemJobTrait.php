e1766ba238a1bd2ee0bd9541f8129b7ed380161f
marco76tv <marco.sottana@gmail.com>
2020-01-16 12:46:28 +0100

    .

--- Traits/CrudContainerItemJobTrait.php
+++ Traits/CrudContainerItemJobTrait.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Str;
 
 trait CrudContainerItemJobTrait {
 	public function __call($name, $arg){
-        $func='\Modules\Xot\Jobs\Crud\\'.$name.'Job';
+        $func='\Modules\Xot\Jobs\Crud\\'.Str::studly($name).'Job';
         $panel=$func::dispatchNow($arg[1],$arg[2]);
         return $panel;
     }
