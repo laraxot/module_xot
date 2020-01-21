ed3b33e5360eccdf6cd8fb4d8be92befe4ec105c
marco76tv <marco.sottana@gmail.com>
2020-01-21 12:20:54 +0100

    .

--- Http/Controllers/XotBaseController.php
+++ Http/Controllers/XotBaseController.php
@@ -13,7 +13,7 @@ abstract class XotBaseController extends Controller
 
     public function __call($name, $arg)
     {
-        $func  = '\Modules\Xot\Jobs\Crud\\' . Str::kebab($name) . 'Job';
+        $func  = 'Modules\Xot\Jobs\Crud\\' . Str::studly($name) . 'Job';
         $panel = $func::dispatchNow($arg[1], $arg[2]);
         return $panel;
     }
