ea9159b53fb5a26a7f227de8d8cfb1a449e716f4
marco76tv <marco.sottana@gmail.com>
2020-02-17 13:45:29 +0100

    .

--- Services/ImageService.php
+++ Services/ImageService.php
@@ -217,8 +217,11 @@ class ImageService {
         $basename = Str::before($basename, '?');
         self::$filename = public_path('/imgz/'.self::$width.'x'.self::$height.'/'.$basename);
         \File::makeDirectory(\dirname(self::$filename), 0775, true, true);
-        $r = self::$img->save(self::$filename, 75);
+        try{
+            $r = self::$img->save(self::$filename, 75);
+        }catch(\Exception $e){
 
+        }
         return self::getInstance();
     }
 
