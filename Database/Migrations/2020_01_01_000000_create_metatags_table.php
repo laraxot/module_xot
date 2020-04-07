d7cfe68cf554d30c6a2c634a4033eed2ff279ea7
marco76tv <marco.sottana@gmail.com>
2020-04-07 13:13:29 +0200

    .

---
+++ Database/Migrations/2020_01_01_000000_create_metatags_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+//----- bases ----
+use Modules\Xot\Database\Migrations\XotBaseMigration;
+//----- models -----
+use Modules\Xot\Models\Metatag as MyModel;
+
+class CreateImageTable extends XotBaseMigration {
+    public function up() {
+        //-- CREATE --
+        if (! $this->tableExists()) {
+            $this->getConn()->create($this->getTable(), function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('title')->nullable();
+                $table->text('subtitle')->nullable();
+                $table->string('charset')->nullable();
+                $table->string('author')->nullable();
+                $table->text('meta_description')->nullable();
+                $table->text('meta_keywords')->nullable();
+                $table->text('logo_src')->nullable();
+                $table->text('logo_footer_src')->nullable();
+                $table->string('tennant_name')->nullable();
+            });
+        }//end create
+
+        //-- UPDATE --
+        $this->getConn()->table($this->getTable(), function (Blueprint $table) {
+            if (! $this->hasColumn('updated_at')) {
+                $table->timestamps();
+            }
+            if (! $this->hasColumn('updated_by')) {
+                $table->string('updated_by')->nullable()->after('updated_at');
+                $table->string('created_by')->nullable()->after('created_at');
+            }
+        }); //end update
+    }
+}
---
+++ Models/Metatag.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Modules\Xot\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+//------ ext models---
+
+class Metatag extends Model {
+
+}
