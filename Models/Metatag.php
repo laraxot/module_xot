a566bad84a73077ad2a8dd7e8a49ae8fdd4d3817
Malebestia <malebestia@yahoo.it>
2020-05-29 21:06:15 +0200

    .

--- Models/Metatag.php
+++ Models/Metatag.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 //------ ext models---
 
-class Metatag extends Model {
+class Metatag extends BaseModel {
     protected $fillable=['id','title','subtitle','charset','author','meta_description','meta_keywords','logo_src','logo_footer_src','tennant_name','created_at','created_by','updated_at','updated_by'];
 
 
--- Models/Panels/Actions/StoreFileMetatagAction.php
+++ Models/Panels/Actions/StoreFileMetatagAction.php
@@ -1,22 +1,23 @@
 <?php
+
 namespace Modules\Xot\Models\Panels\Actions;
 
 use Illuminate\Support\Facades\File;
-use Illuminate\Support\Str;
 
 class StoreFileMetatagAction extends XotBasePanelAction {
-     public $onContainer = false; //per tutte le righe, esempio xls
+    public $onContainer = false; //per tutte le righe, esempio xls
      public $onItem = true; //per riga selezionata
      public $icon = '<i class="far fa-save"></i>';
 
-     public function handle(){
-        $data=$this->row->toArray();
-        $content=var_export($data, true);
-        $filename=base_path('config/'.$this->row->tennant_name.'/metatag.php');
-        $filename=str_replace(['\\','/'],[DIRECTORY_SEPARATOR,DIRECTORY_SEPARATOR],$filename);
+    public function handle() {
+        $data = $this->row->toArray();
+        $content = var_export($data, true);
+        $filename = base_path('config/'.$this->row->tennant_name.'/metatag.php');
+        $filename = str_replace(['\\', '/'], [DIRECTORY_SEPARATOR, DIRECTORY_SEPARATOR], $filename);
         //return $filename;
-        $content='<'.'?php'.chr(13).'return '.$content.';';
+        $content = '<'.'?php'.chr(13).'return '.$content.';';
         File::put($filename, $content);
 
-     }
+        return '<h3>+Done</h3>';
+    }
 }
