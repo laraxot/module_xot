a37dacc39d7f104e444ba823d61a0f679071456e
marco76tv <marco.sottana@gmail.com>
2020-06-03 19:40:24 +0200

    .

--- Http/Controllers/HomeController.php
+++ Http/Controllers/HomeController.php
@@ -5,14 +5,18 @@ namespace Modules\Xot\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 //---- services ---
-use Modules\Blog\Models\Home;
+//use Modules\Blog\Models\Home;
 use Modules\Theme\Services\ThemeService;
 use Modules\Xot\Services\PanelService as Panel;
+use Modules\Xot\Services\TenantService as Tenant;
 
 class HomeController extends Controller {
     public function index(Request $request) {
+        $home =Tenant::modelEager('home')->firstOrCreate(['id' => 1]);
+        /*
         $home = Home::with('post')
             ->firstOrCreate(['id' => 1]);
+            */
         $home_panel = Panel::get($home);
 
         if ('' != $request->_act) {
@@ -26,34 +30,10 @@ class HomeController extends Controller {
     }
 
     public function show(Request $request) {
-        //dddx(app()->getLocale());
-        $home = Home::with('post')
+        $home = Tenant::modelEager('home')
         ->firstOrCreate(['id' => 1]);
-
         $panel = Panel::get($home);
-
         return $panel->out();
-
-        /*
-        $lang = app()->getLocale();
-        $guid = 'chicken-taste-restaurant';
-        if (1) {
-            $item = \Modules\Food\Models\Restaurant::with('post')
-            ->whereHas('post',
-                function ($query) use ($guid,$lang) {
-                    $query->where('guid', 'like', $guid)
-                        ->where('lang', $lang);
-                }
-            )->first();
-        } else {
-            $post = \Modules\Blog\Models\Post::where('guid', 'like', $guid)
-            ->where('lang', $lang)->first();
-            $item = $post->linkable;
-        }
-        $panel = Panel::get($item);
-
-        return $panel->out();
-        //*/
     }
 
     public function redirect(Request $request) {
