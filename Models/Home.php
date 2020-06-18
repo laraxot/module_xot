2f9e59b233cbe0231214211f10ed312426a690dd
marco76tv <marco.sottana@gmail.com>
2020-06-18 15:39:18 +0200

    widget relationship

--- Models/Home.php
+++ Models/Home.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 //------ ext models---
 
 class Home extends Model {
-    protected $fillable = [''];
+    protected $fillable = ['id', 'article_type', 'icon_src'];
+
+    //--------- relationship ---------------
+    public function widgets() {
+        return $this->morphMany(Widget::class, 'post')->orderBy('pos');
+    }
 }
