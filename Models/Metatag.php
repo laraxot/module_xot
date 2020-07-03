c8bfd231cfe401357bb16623fe1ba7b44a45e1c8
Malebestia <malebestia@yahoo.it>
2020-07-03 20:03:17 +0200

    .

--- Models/Metatag.php
+++ Models/Metatag.php
@@ -2,12 +2,10 @@
 
 namespace Modules\Xot\Models;
 
-use Illuminate\Database\Eloquent\Model;
-
 //------ ext models---
 
 class Metatag extends BaseModel {
-    protected $fillable=['id','title','subtitle','charset','author','meta_description','meta_keywords','logo_src','logo_footer_src','tennant_name','created_at','created_by','updated_at','updated_by'];
-
-
+    protected $fillable = ['id', 'title', 'subtitle', 'charset', 'author',
+        'meta_description', 'meta_keywords', 'logo_src', 'logo_footer_src',
+        'tennant_name', 'sitename', 'created_at', 'created_by', 'updated_at', 'updated_by', ];
 }
--- Models/Panels/MetatagPanel.php
+++ Models/Panels/MetatagPanel.php
@@ -1,233 +1,223 @@
 <?php
+
 namespace Modules\Xot\Models\Panels;
 
-use Illuminate\Support\Str;
 use Illuminate\Http\Request;
-use Illuminate\Database\Eloquent\Builder;
 
 //--- Services --
-use Modules\Xot\Services\StubService;
-use Modules\Xot\Services\RouteService;
-
-
-use Modules\Xot\Models\Panels\XotBasePanel;
 
 class MetatagPanel extends XotBasePanel {
-	/**
-	 * The model the resource corresponds to.
-	 *
-	 * @var string
-	 */
-	public static $model = 'Modules\Xot\Models\Metatag';
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = 'Modules\Xot\Models\Metatag';
 
-	/**
-	 * The single value that should be used to represent the resource when being displayed.
-	 *
-	 * @var string
-	 */
-	public static $title = "title"; 
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'title';
 
-	/**
-	 * The columns that should be searched.
-	 *
-	 * @var array
-	 */
-	public static $search = array (
-) ;
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+    ];
 
-	/**
-	* The relationships that should be eager loaded on index queries.
-	*
-	* @var array
-	*/
-	public static function with(){
-	  return [];
-	}
+    /**
+     * The relationships that should be eager loaded on index queries.
+     *
+     * @var array
+     */
+    public static function with() {
+        return [];
+    }
 
-	public function search(){
-		return [];
-	}
+    public function search() {
+        return [];
+    }
 
-	/**
-	 * on select the option id
-	 *
-	 */
+    /**
+     * on select the option id.
+     */
+    public function optionId($row) {
+        return $row->area_id;
+    }
 
-	public function optionId($row){
-		return $row->area_id;
-	}
+    /**
+     * on select the option label.
+     */
+    public function optionLabel($row) {
+        return $row->area_define_name;
+    }
 
-	/**
-	 * on select the option label 
-	 *
-	 */
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return array
+        'col_bs_size' => 6,
+        'sortable' => 1,
+        'rules' => 'required',
+        'rules_messages' => ['it'=>['required'=>'Nome Obbligatorio']],
+        'value'=>'..',
+     */
+    public function indexNav() {
+        return null;
+    }
 
-	public function optionLabel($row){
-		return $row->area_define_name;
-	}
+    /**
+     * Build an "index" query for the given resource.
+     *
+     * @param Request                               $request
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public static function indexQuery($data, $query) {
+        //return $query->where('auth_user_id', $request->user()->auth_user_id);
+        return $query;
+    }
 
-	/**
-	 * Get the fields displayed by the resource.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @return array
+    /**
+     * Build a "relatable" query for the given resource.
+     *
+     * This query determines which instances of the model may be attached to other resources.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public static function relatableQuery(Request $request, $query) {
+        //return $query->where('auth_user_id', $request->user()->auth_user_id);
+         //return $query->where('user_id', $request->user()->id);
+    }
 
-		'col_bs_size' => 6,
-		'sortable' => 1,
-		'rules' => 'required',
-		'rules_messages' => ['it'=>['required'=>'Nome Obbligatorio']],
-		'value'=>'..',
+    public function fields() {
+        return [
+            (object) [
+                'type' => 'Id',
+                'name' => 'id',
+                'comment' => null,
+            ],
 
-	 */
-	public function indexNav(){
-		return null;
-	}
+            (object) [
+                'type' => 'String',
+                'name' => 'title',
+                'comment' => null,
+            ],
 
-	/**
-	 * Build an "index" query for the given resource.
-	 *
-	 * @param  Request  $request
-	 * @param  \Illuminate\Database\Eloquent\Builder  $query
-	 * @return \Illuminate\Database\Eloquent\Builder
-	 */
+            (object) [
+                'type' => 'Text',
+                'name' => 'subtitle',
+                'comment' => null,
+            ],
 
-	public static function indexQuery($data, $query){
-		//return $query->where('auth_user_id', $request->user()->auth_user_id);
-		return $query; 
-	}
+            (object) [
+                'type' => 'String',
+                'name' => 'charset',
+                'comment' => null,
+            ],
 
-	/**
-	 * Build a "relatable" query for the given resource.
-	 *
-	 * This query determines which instances of the model may be attached to other resources.
-	 *
-	 * @param  Request  $request
-	 * @param  \Illuminate\Database\Eloquent\Builder  $query
-	 * @return \Illuminate\Database\Eloquent\Builder
-	 */
-	public static function relatableQuery(Request $request, $query){
-		//return $query->where('auth_user_id', $request->user()->auth_user_id);
-		 //return $query->where('user_id', $request->user()->id);
-	}
+            (object) [
+                'type' => 'String',
+                'name' => 'author',
+                'comment' => null,
+            ],
 
+            (object) [
+                'type' => 'Text',
+                'name' => 'meta_description',
+                'comment' => null,
+            ],
 
+            (object) [
+                'type' => 'Text',
+                'name' => 'meta_keywords',
+                'comment' => null,
+            ],
 
-	public function fields(){
-		return array (
-  
-  (object) array(
-     'type' => 'Id',
-     'name' => 'id',
-     'comment' => NULL,
-  ),
-  
-  (object) array(
-     'type' => 'String',
-     'name' => 'title',
-     'comment' => NULL,
-  ),
-  
-  (object) array(
-     'type' => 'Text',
-     'name' => 'subtitle',
-     'comment' => NULL,
-  ),
-  
-  (object) array(
-     'type' => 'String',
-     'name' => 'charset',
-     'comment' => NULL,
-  ),
-  
-  (object) array(
-     'type' => 'String',
-     'name' => 'author',
-     'comment' => NULL,
-  ),
-  
-  (object) array(
-     'type' => 'Text',
-     'name' => 'meta_description',
-     'comment' => NULL,
-  ),
-  
-  (object) array(
-     'type' => 'Text',
-     'name' => 'meta_keywords',
-     'comment' => NULL,
-  ),
-  
-  (object) array(
-     'type' => 'Image',
-     'name' => 'logo_src',
-     'comment' => NULL,
-  ),
-  
-  (object) array(
-     'type' => 'Text',
-     'name' => 'logo_footer_src',
-     'comment' => NULL,
-  ),
-  
-  (object) array(
-     'type' => 'String',
-     'name' => 'tennant_name',
-     'comment' => NULL,
-  ),
-  
-);
-	}
-	 
-	/**
-	 * Get the tabs available 
-	 *
-	 * @return array  
-	 */
-	public function tabs(){
-		$tabs_name = [];
-		return $tabs_name;
-	}
-	/**
-	 * Get the cards available for the request.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @return array
-	 */
-	public function cards(Request $request){
-		return [];
-	}
+            (object) [
+                'type' => 'Image',
+                'name' => 'logo_src',
+                'comment' => null,
+            ],
 
-	/**
-	 * Get the filters available for the resource.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @return array
-	 */
-	public function filters(Request $request=null){
-		return [];
-	}
+            (object) [
+                'type' => 'Text',
+                'name' => 'logo_footer_src',
+                'comment' => null,
+            ],
 
-	/**
-	 * Get the lenses available for the resource.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @return array
-	 */
-	public function lenses(Request $request){
-		return [];
-	}
+            (object) [
+                'type' => 'String',
+                'name' => 'tennant_name',
+                'comment' => null,
+            ],
+            (object) [
+                'type' => 'String',
+                'name' => 'sitename',
+                'comment' => null,
+            ],
+        ];
+    }
 
-	/**
-	 * Get the actions available for the resource.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @return array
-	 */
-	public function actions(Request $request=null){
-		return [
-			new Actions\StoreFileMetatagAction(),
+    /**
+     * Get the tabs available.
+     *
+     * @return array
+     */
+    public function tabs() {
+        $tabs_name = [];
 
-		];
-	}
+        return $tabs_name;
+    }
 
+    /**
+     * Get the cards available for the request.
+     *
+     * @return array
+     */
+    public function cards(Request $request) {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return array
+     */
+    public function filters(Request $request = null) {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @return array
+     */
+    public function lenses(Request $request) {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return array
+     */
+    public function actions(Request $request = null) {
+        return [
+            new Actions\StoreFileMetatagAction(),
+        ];
+    }
 }
