4f5ee77dc6314dbb6033be4820a859c9297df938
marco xot <marco.sottana@gmail.com>
2020-09-21 23:01:06 +0200

    .

--- Http/Controllers/Container0/Container1/Container2/Container3/Container4Controller.php
+++ Http/Controllers/Container0/Container1/Container2/Container3/Container4Controller.php
@@ -2,7 +2,6 @@
 
 namespace Modules\Xot\Http\Controllers\Container0\Container1\Container2\Container3;
 
-//use Modules\Xot\Traits\CrudContainerItemNoPostTrait as CrudTrait;
 use Modules\Xot\Http\Controllers\XotBaseContainerController;
 
 class Container4Controller extends XotBaseContainerController {
