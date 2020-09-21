cb3b14e01ed0838552481fe264eb223e18e9d531
marco xot <marco.sottana@gmail.com>
2020-09-21 19:17:44 +0200

    .

--- Http/Controllers/Admin/ModuleController.php
+++ Http/Controllers/Admin/ModuleController.php
@@ -3,6 +3,8 @@
 namespace Modules\Xot\Http\Controllers\Admin;
 
 //use Modules\Xot\Traits\CrudContainerItemNoPostTrait as CrudTrait;
-
+/*
+* gestisce i module
+*/
 class ModuleController extends XotBaseContainerController {
 }
