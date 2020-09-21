27d4801c88cd3ab11510e5a79cc9039a342cc4ce
marco xot <marco.sottana@gmail.com>
2020-09-21 23:05:58 +0200

    .

--- Http/Controllers/Container0/Container1Controller.php
+++ Http/Controllers/Container0/Container1Controller.php
@@ -4,5 +4,8 @@ namespace Modules\Xot\Http\Controllers\Container0;
 
 use Modules\Xot\Http\Controllers\XotBaseContainerController;
 
+/*
+* container0.container1
+*/
 class Container1Controller extends XotBaseContainerController {
 }
