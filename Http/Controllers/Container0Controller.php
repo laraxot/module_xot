c7117c7ad0d3205468976fcf21d93df7d4ebf871
marco xot <marco.sottana@gmail.com>
2020-09-21 22:58:40 +0200

    .

--- Http/Controllers/Container0Controller.php
+++ Http/Controllers/Container0Controller.php
@@ -2,7 +2,5 @@
 
 namespace Modules\Xot\Http\Controllers;
 
-//use Modules\Xot\Traits\CrudContainerItemNoPostTrait as CrudTrait;
-
 class Container0Controller extends XotBaseContainerController {
 }
