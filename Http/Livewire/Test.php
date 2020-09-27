7981650e66bae1c1701a468b118d8ba76926e3d6
marco76tv <marco.sottana@gmail.com>
2020-09-27 15:23:37 +0200

    .

--- Http/Livewire/Test.php
+++ Http/Livewire/Test.php
@@ -8,9 +8,27 @@ use Livewire\Component;
 class Test extends Component {
     public $animal = '';
     public $options;
+    public $products=[];
+    public $change_cats=[];
+    public $changes=[];
 
     public function mount() {
         $this->options = ['one' => true, 'two' => false, 'three' => false];
+        $this->products=[
+            (object)['id'=>1,'title'=>'Margherita'],
+            (object)['id'=>2,'title'=>'Capricciosa'],
+        ];
+        $this->change_cats=[
+            (object)['id'=>1,'title'=>'Formaggi'],
+            (object)['id'=>2,'title'=>'Salumi'],
+        ];
+        $this->changes=[
+            (object)['id'=>1,'id_cat'=>1,'title'=>'mozzarella'],
+            (object)['id'=>2,'id_cat'=>1,'title'=>'gorgonzola'],
+            (object)['id'=>3,'id_cat'=>2,'title'=>'salame'],
+            (object)['id'=>4,'id_cat'=>2,'title'=>'prosciutto'],
+        ];
+
     }
 
     public function render() {
--- Resources/views/livewire/test.blade.php
+++ Resources/views/livewire/test.blade.php
@@ -4,7 +4,7 @@
     <input wire:model="animal" name="animal" type="radio" value="dogs" /> Dogs
     <input wire:model="animal" name="animal" type="radio" value="both" /> Both
     [{{ $animal }}]
-
+    <hr/>
     {{--
         https://forum.laravel-livewire.com/t/unable-to-get-checkbox-value/157/7
     --}}
@@ -23,7 +23,29 @@
     @foreach($options as $key=>$option)
         {{$key }}: {{($option) ? 'true' : 'false' }}<br />
     @endforeach
+    <hr/>
 
 
+    @foreach($products as $product)
+        <h3>{{ $product->title }}</h3>
+        @foreach($change_cats as $change_cat)
+            <h4>{{ $change_cat->title }}</h4>
+
+            <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                <label class="btn btn-secondary">
+                    <input type="radio"  name="changes[]"  autocomplete="off" value="-1"> -
+                </label>
+                <label class="btn btn-secondary active" >
+                    <input type="radio"  name="changes[]"  autocomplete="off"  checked value="0"> &nbsp;
+                </label>
+                <label class="btn btn-secondary">
+                    <input type="radio"  name="changes[]" autocomplete="off" value="1"> +
+                </label>
+            </div>
+        @endforeach
+    @endforeach
+        <pre>
+            {{ print_r($changes) }}
+        </pre>
 
 </div>
