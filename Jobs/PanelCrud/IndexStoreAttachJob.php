13b14f440ceffa65a5bb42601cc24cec763af885
marco76tv <marco.sottana@gmail.com>
2020-09-30 11:19:44 +0200

    .

--- Jobs/PanelCrud/IndexStoreAttachJob.php
+++ Jobs/PanelCrud/IndexStoreAttachJob.php
@@ -11,7 +11,8 @@ use Illuminate\Queue\SerializesModels;
 //----------- Requests ----------
 //------------ services ----------
 
-class IndexStoreAttachJob implements ShouldQueue {
+class IndexStoreAttachJob implements ShouldQueue
+{
     use Dispatchable;
     use InteractsWithQueue;
     use Queueable;
@@ -26,7 +27,8 @@ class IndexStoreAttachJob implements ShouldQueue {
 
     protected $panel;
 
-    public function __construct($request, $panel) {
+    public function __construct($request, $panel)
+    {
         $this->panel = $panel;
         $this->data = $request->all();
     }
@@ -36,10 +38,37 @@ class IndexStoreAttachJob implements ShouldQueue {
      *
      * @return void
      */
-    public function handle() {
+    public function handle()
+    {
         //dddx([$this->panel, $this->data]);
-        $this->panel->rows->attach($this->data['groups']['to']);
+        //$this->panel->rows->attach($this->data['groups']['to']);
         //return 'preso';
+        $to=$this->data['groups']['to'];
+        if (!is_array($to)) {
+            $to=[];
+        }
+        $related=$this->panel->rows->getRelated();
+        $items_key = $related->getKeyName();
+
+        $items_0 = $this->panel->rows->get()->pluck($items_key);
+
+        
+        $items_1 = collect($to);
+        $items_add = $items_1->diff($items_0);
+        $items_sub = $items_0->diff($items_1);
+        //$items->detach($items_sub->all());
+
+        //dddx(['add'=>$items_add,'sub'=>$items_sub]);
+        $this->panel->rows->detach($items_sub->all());
+        $this->panel->rows->attach($items_add->all());
+
+        $status = 'collegati ['.\implode(', ', $items_add->all()).'] scollegati ['.\implode(', ', $items_sub->all()).']';
+        \Session::flash('status', $status);
+
+
+
+
+
 
         return $this->panel;
     }
